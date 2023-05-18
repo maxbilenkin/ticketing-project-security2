74e21e598530c176c9f6f2124944507e6e708012
Maxim <maxbilenkin@gmail.com>
2023-05-17 22:26:38 -0400

    Security

---
+++ src/test/java/com/cydeo/ticketingprojectsecurity2/TicketingProjectSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingprojectsecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectSecurity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
